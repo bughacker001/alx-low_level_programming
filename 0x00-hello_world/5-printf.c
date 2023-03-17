a2f1a3785478e2bb4d23749bdcbae5b3f05cb36e
bughacker <bughackerjanaan@yahoo.com>
2023-03-16 20:45:29 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#!/bin/bash
+#include <studio.h>
+/* program to print using the printf *
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
