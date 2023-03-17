627a47315659519925d24142e85f6b6b168d0e2f
bughacker <bughackerjanaan@yahoo.com>
2023-03-16 21:43:51 -0700

    101-quote.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * program to of various sizes of a computer 
+ * Return: 0 (Success)
+ */
+
+int main (void)
+Â	char a;
+	int b;
+	
+	long int c;
+	long long int d;
+	float eÂ;
+printf("Size of char: %1u byte(s)\n", (unsigned long)$sizeof(a));
