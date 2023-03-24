1f093290d242e7356a9dfa4d57483c1658698a29
bughacker <bughackerjanaan@yahoo.com>
2023-03-23 17:23:15 -0700

    finishing

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_numbers - print 0-9
  * return: void
@@ -11,7 +10,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+	_putchar(c + '0' );
 	}
 	_putchar('\n');
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+Alx-low_level_programming
+0x03-debugging
+0x03-debugging
