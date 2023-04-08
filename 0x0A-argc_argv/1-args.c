4fa121c42d8f51a63aa82542abd9cb2762303681
bughacker <bughackerjanaan@yahoo.com>
2023-04-08 09:15:22 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-
-int main (int argc, char *argv[])
+#include <stdlib.h>
+/**
+ * main - Number of arguments passed into it.b
+ * @argc: size of argv array and number of command line arg
+ * @argv: An array of size argc
+ * Return: 0
+ */
+int main(int argc, char **argv __attribute__((unused)))
+//int main (int argc, char *argv[])
 {
   int count;
-
-  printf ("This program was called with \"%s\".\n",argv[0]);
-
+  printf("This program was called with \"%s\".\n",argv[0]);
   if (argc > 1)
     {
       for (count = 1; count < argc; count++)
@@ -18,5 +23,5 @@ int main (int argc, char *argv[])
       printf("The command had no other arguments.\n");
     }
 
-  return 0;
+  return (0);
 }
