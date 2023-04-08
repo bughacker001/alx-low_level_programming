35d17f227c9707c31ce3104b0a60a262ed15cdbd
bughacker <bughackerjanaan@yahoo.com>
2023-04-08 10:51:42 -0700

    betty fault

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - Prints all arguments it receives.
- * @argc: size of argv array and number of command line
+ *
+ * @argc: size of argv array and number of command line arguments
+ *
  * @argv: An array of size argc
+ *
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	int b;
-	for (b = 0; b < argc; b++)
+	int i;
+
+	for (i = 0; i < argc; i++)
 	{
-	printf("%s\n", argv[b]);
+	printf("%s\n", argv[i]);
 	}
+
 	return (0);
 }
