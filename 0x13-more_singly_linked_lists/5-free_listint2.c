dab8fc29f8c567b96aca4c2576022840fd96af26
bughacker <bughackerjanaan@yahoo.com>
2023-05-01 01:25:47 -0700

    code for 59free_list2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - is use to free a listint_t list not forgetting to set the head to NULL
+ * @head: a pointer to pointer to head of a list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	/* Traversing the list and freeing each node */
+	/* introducing the while loop */
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	/* Setting head to NULL */
+	*head = NULL;
+}
