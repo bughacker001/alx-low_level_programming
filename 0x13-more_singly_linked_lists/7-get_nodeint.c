17606945e4e1317842cc5dc83e10a7f3b18d604e
bughacker <bughackerjanaan@yahoo.com>
2023-05-01 08:31:23 -0700

    fixing 7-get_nodeint.c error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,34 +2,24 @@
 
 /**
  * pop_listint - deletes the head node of a listint_t linked list,
- * and returns the head node’s data (n).
- * @head: pointer to the head of the list
+ * and returns the head node's data (n).
+ * @head: pointer to a pointer to the head node of the list
  *
- * Return: the data stored in the head node, or 0 if the list is empty
+ * Return: the head node's data (n), or 0 if the list is empty
  */
 int pop_listint(listint_t **head)
 {
-    int data;
+    int n;
     listint_t *temp;
 
-    /* Check if the list is empty */
-    if (*head == NULL) {
-        return 0;
-    }
+    if (*head == NULL)
+        return (0);
 
-    /* Store the head node in a temporary variable */
     temp = *head;
-
-    /* Set the head of the list to the next node */
+    n = temp->n;
     *head = (*head)->next;
-
-    /* Retrieve the data stored in the original head node */
-    data = temp->n;
-
-    /* Free the memory allocated for the original head node */
     free(temp);
 
-    /* Return the data stored in the original head node */
-    return data;
+    return (n);
 }
 
